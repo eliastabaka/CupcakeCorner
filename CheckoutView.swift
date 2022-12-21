7a384adbb53cd5203e23c939ee78493738baeb63
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-12-21 23:50:30 +0000

    Checking for a valid address

---
+++ CheckoutView.swift
@@ -0,0 +1,24 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Miłosz Tabaka on 21/12/2022.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
