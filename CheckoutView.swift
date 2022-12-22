9ce6cb5efa545f876037b7aa27ebc7ed423e5654
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-12-22 23:49:21 +0000

    Day 52  Challenge 3

--- CheckoutView.swift
+++ CheckoutView.swift
@@ -27,7 +27,7 @@ struct CheckoutView: View {
                 }
                 .frame(height: 233)
                 
-                Text("Your total is \(order.cost, format: .currency(code: "USD"))")
+                Text("Your total is \(order.data.cost, format: .currency(code: "USD"))")
                     .font(.title)
 
                 Button("Place Order") {
@@ -67,7 +67,7 @@ struct CheckoutView: View {
             let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
             
             let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
-            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            confirmationMessage = "Your order for \(decodedOrder.data.quantity)x \(Data.types[decodedOrder.data.type].lowercased()) cupcakes is on its way!"
             showingConfirmation = true
         } catch {
             showingErrorAllert = true
--- Order.swift
+++ Order.swift
@@ -7,17 +7,12 @@
 
 import SwiftUI
 
-class Order: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
-    }
-    
+struct Data: Codable {
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    var type = 0
+    var quantity = 3
     
-    @Published var type = 0
-    @Published var quantity = 3
-    
-    @Published var specialRequestEnabled = false {
+    var specialRequestEnabled = false {
         didSet {
             if specialRequestEnabled == false {
                 extraFrosting = false
@@ -26,13 +21,13 @@ class Order: ObservableObject, Codable {
         }
     }
     
-    @Published var extraFrosting = false
-    @Published var addSprinkles = false
+    var extraFrosting = false
+    var addSprinkles = false
     
-    @Published var name = ""
-    @Published var streetAddress = ""
-    @Published var city = ""
-    @Published var zip = ""
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
     
     var hasValidAddress: Bool {
         if name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || streetAddress.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || city.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty || zip.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
@@ -61,36 +56,26 @@ class Order: ObservableObject, Codable {
 
         return cost
     }
+}
+
+class Order: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case data
+    }
     
+    @Published var data = Data()
+        
     init() { }
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         
-        try container.encode(type, forKey: .type)
-        try container.encode(quantity, forKey: .quantity)
-
-        try container.encode(extraFrosting, forKey: .extraFrosting)
-        try container.encode(addSprinkles, forKey: .addSprinkles)
-
-        try container.encode(name, forKey: .name)
-        try container.encode(streetAddress, forKey: .streetAddress)
-        try container.encode(city, forKey: .city)
-        try container.encode(zip, forKey: .zip)
+        try container.encode(data, forKey: .data)
     }
     
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        type = try container.decode(Int.self, forKey: .type)
-        quantity = try container.decode(Int.self, forKey: .quantity)
-
-        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
-        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
-
-        name = try container.decode(String.self, forKey: .name)
-        streetAddress = try container.decode(String.self, forKey: .streetAddress)
-        city = try container.decode(String.self, forKey: .city)
-        zip = try container.decode(String.self, forKey: .zip)
+        data = try container.decode(Data.self, forKey: .data)
     }
 }
